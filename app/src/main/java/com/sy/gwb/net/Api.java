d0a62d155e8b7f0338b5309295e767ab39d71b0d
gongwenbo <1071259226@qq.com>
2018-03-22 11:01:00 +0800

    单例模式优化

--- app/src/main/java/com/sy/gwb/net/Api.java
+++ app/src/main/java/com/sy/gwb/net/Api.java
@@ -1,23 +1,27 @@
 package com.sy.gwb.net;
 
 /**
+ * Java中静态内部类可以访问其外部类的成员属性和方法
+ * 同时，静态内部类只有当被调用的时候才开始首次被加载，利用此特性
+ * 可以实现懒汉式，在静态内部类中静态初始化外部类的单一实例即可。
  * Created by ${GongWenbo} on 2018/3/19 0019.
  */
 
 public class Api {
 
-    private static ApiService sApiService;
-//    private static final String BASE_URL = "http://apis.juhe.cn/mobile/";
-    // 赳赳单车
-    private static final String BASE_URL = "http://139.196.194.172:8083/ElectricBicyclePro/";
+    private static final String BASE_URL = "http://apis.juhe.cn/mobile/";
+
+    private Api() {
+
+    }
+
+    // 利用静态内部类特性实现外部类的单例
+    private static class ApiInterior {
+        private static ApiService sApiService = new ApiBuilder().getRetrofitBuilder().baseUrl(BASE_URL).build().create(ApiService.class);
+    }
 
     public static ApiService getInstance() {
-        if (sApiService == null) {
-            synchronized (Api.class) {
-                sApiService = ApiBuilder.getRetrofitBuilder().baseUrl(BASE_URL).build().create(ApiService.class);
-            }
-        }
-        return sApiService;
+        return ApiInterior.sApiService;
     }
 
 }
